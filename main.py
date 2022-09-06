57c63fc9c5705bf62865a51449f17ed0fe58ed2a
Kovalev Vladimir <wault@mail.ru>
2022-09-06 12:05:11 +0500

    попытка с fake ua

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import requests
 import bs4
-from kiss_headers import parse_it
+from pprint import pprint
+from fake_user_agent import user_agent
 
+ua = user_agent()
+pprint(ua)
 url = "https://habr.com/ru/all/"
 
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
-response = requests.get(url)
-headers = parse_it(response)
-headers.content_type.charset  # output: ISO-8859-1
+KEYWORDS = ['дизайн', 'фото', 'web', 'python','Big Data', 'Машинное обучение']
 
+response = requests.get(url, headers=ua)
 text = response.text
-
 soup = bs4.BeautifulSoup(text, features="html.parser")
-print(headers)
+
+# pprint(soup)
+
+articles = soup.find_all("article")
+pprint(articles)
+
+for article in articles:
+    keywords = article.find_all(class_="tm-article-snippet__hubs-item")
+    keywords = [keyword.text.strip() for keyword in keywords]
+    pprint(keywords)
+    print()
