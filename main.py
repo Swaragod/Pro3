397b2daae48924036f60e27fc93447a06f6acc50
Kovalev Vladimir <wault@mail.ru>
2022-09-07 11:18:07 +0500

    Финальная версия

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import requests
 import bs4
-from pprint import pprint
-from fake_user_agent import user_agent
+from fake_headers import Headers
 
-ua = user_agent()
-pprint(ua)
+HEADERS = Headers(os="mac", headers=True).generate()
+KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'В']
+
 url = "https://habr.com/ru/all/"
+url_base = "https://habr.com"
 
-
-KEYWORDS = ['дизайн', 'фото', 'web', 'python','Big Data', 'Машинное обучение']
-
-response = requests.get(url, headers=ua)
+response = requests.get(url, headers=HEADERS)
 text = response.text
+
 soup = bs4.BeautifulSoup(text, features="html.parser")
 
-
-# pprint(soup)
-
 articles = soup.find_all("article")
-pprint(articles)
-
 for article in articles:
-    keywords = article.find_all(class_="tm-article-snippet__hubs-item")
-    keywords = [keyword.text.strip() for keyword in keywords]
-    pprint(keywords)
-    print()
+    previews = article.find_all(class_="tm-article-body tm-article-snippet__lead")
+    previews = [preview.text.strip() for preview in previews]
+    for preview in previews:
+        for keyword in KEYWORDS:
+            if keyword in preview:
+                post_date = article.find("time").attrs["title"][0:10]
+                title = article.find("h2").find("span").text
+                href = article.find(class_="tm-article-snippet__title-link").attrs["href"]
+                link = url_base + href
+                print(f'<{post_date}> - <{title}> - <{link}>')
 
 
-git
